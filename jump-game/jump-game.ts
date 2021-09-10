c3d91c986b090e2a07288d17d2018baee7f2e1b2
Eric Bayasgalan <eric.bayasgalan@gmail.com>
2021-09-09 20:57:29 -0500

    Time: 88 ms (89.12%), Space: 43.4 MB (93.20%) - LeetHub

---
+++ jump-game/jump-game.ts
@@ -0,0 +1,9 @@
+const canJump = (nums: number[]):boolean => {
+    let lastGoodIndex = nums.length - 1;
+    for(let i = nums.length - 1; i >= 0; i--) {
+        if(i + nums[i] >= lastGoodIndex) {
+            lastGoodIndex = i;
+        }
+    }
+    return lastGoodIndex === 0;
+};
